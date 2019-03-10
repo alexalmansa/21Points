a6e0adf088a8f748d56c16bd91251aec83843a17
mac12llm <mac12llm@gmail.com>
2019-03-10 11:16:54 +0100

    Strings.xml update, now used

--- app/src/main/java/com/marcllort/a21points/MainActivity.java
+++ app/src/main/java/com/marcllort/a21points/MainActivity.java
@@ -3,40 +3,26 @@ package com.marcllort.a21points;
 import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
-import android.view.Menu;
-import android.view.MenuItem;
+import android.util.Log;
+
 
 public class MainActivity extends AppCompatActivity {
+
+    private static final String TAG = "21Ponts";
+
+    //Farem servir el MainActivity com un gestor de les diferents activitats
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+        Log.d(TAG, "onCreate(Bundle) called");
         setContentView(R.layout.activity_main);
 
 
         Intent intent = new Intent(this, LoginActivity.class);
-        startActivity(intent);
+        Log.d(TAG, "startActivity(intent) created");
+        startActivity(intent);                                                      // Caldra fer startActibityForResult per saber si ha pogut fer login o no
     }
 
-    @Override
-    public boolean onCreateOptionsMenu(Menu menu) {
-        // Inflate the menu; this adds items to the action bar if it is present.
-        getMenuInflater().inflate(R.menu.menu_main, menu);
-        return true;
-    }
 
-    @Override
-    public boolean onOptionsItemSelected(MenuItem item) {
-        // Handle action bar item clicks here. The action bar will
-        // automatically handle clicks on the Home/Up button, so long
-        // as you specify a parent activity in AndroidManifest.xml.
-        int id = item.getItemId();
-
-        //noinspection SimplifiableIfStatement
-        if (id == R.id.action_settings) {
-            return true;
-        }
-
-        return super.onOptionsItemSelected(item);
-    }
 }
