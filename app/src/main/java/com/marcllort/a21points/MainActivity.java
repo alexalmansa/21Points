64f02b3f287897c0001722b79259d5cd67520c95
mac12llm <mac12llm@gmail.com>
2019-03-30 14:19:48 +0100

    Chart added

--- app/src/main/java/com/marcllort/a21points/MainActivity.java
+++ app/src/main/java/com/marcllort/a21points/MainActivity.java
@@ -1,14 +1,34 @@
 package com.marcllort.a21points;
 
 import android.content.Intent;
+import android.graphics.Color;
+import android.graphics.DashPathEffect;
+import android.graphics.drawable.Drawable;
+import android.support.v4.content.ContextCompat;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.util.Log;
+
+import com.github.mikephil.charting.charts.LineChart;
+import com.github.mikephil.charting.components.LimitLine;
+import com.github.mikephil.charting.components.XAxis;
+import com.github.mikephil.charting.components.YAxis;
+import com.github.mikephil.charting.data.Entry;
+import com.github.mikephil.charting.data.LineData;
+import com.github.mikephil.charting.data.LineDataSet;
+import com.github.mikephil.charting.formatter.IFillFormatter;
+import com.github.mikephil.charting.interfaces.dataprovider.LineDataProvider;
+import com.github.mikephil.charting.interfaces.datasets.ILineDataSet;
+import com.github.mikephil.charting.utils.Utils;
+
+import java.util.ArrayList;
 
 
 public class MainActivity extends AppCompatActivity {
 
     private static final String TAG = "21Points";
+    private LineChart chart;
+
 
     //Farem servir el MainActivity com un gestor de les diferents activitats
 
@@ -18,11 +38,124 @@ public class MainActivity extends AppCompatActivity {
         Log.d(TAG, "onCreate(Bundle) called");
         setContentView(R.layout.activity_main);
 
+        chart = findViewById(R.id.chart1);
+        chart.setViewPortOffsets(0, 0, 0, 0);
 
-        Intent intent = new Intent(this, LoginActivity.class);
+        // no description text
+        chart.getDescription().setEnabled(false);
+
+        // enable touch gestures
+        chart.setTouchEnabled(true);
+
+        // enable scaling and dragging
+        chart.setDragEnabled(true);
+        chart.setScaleEnabled(true);
+
+        // if disabled, scaling can be done on x- and y-axis separately
+        chart.setPinchZoom(false);
+
+        chart.setDrawGridBackground(false);
+        chart.setMaxHighlightDistance(300);
+        XAxis x = chart.getXAxis();
+        x.setEnabled(false);
+
+        YAxis y = chart.getAxisLeft();
+        //y.setTypeface(tfLight);
+        y.setLabelCount(6, false);
+        y.setTextColor(Color.WHITE);
+        y.setPosition(YAxis.YAxisLabelPosition.INSIDE_CHART);
+        y.setDrawGridLines(false);
+        y.setAxisLineColor(Color.WHITE);
+
+        chart.getAxisRight().setEnabled(false);
+
+
+
+        chart.getLegend().setEnabled(false);
+
+        chart.animateXY(2000, 2000);
+
+        // don't forget to refresh the drawing
+        chart.invalidate();
+
+
+
+
+
+        // add data
+        //seekBarX.setProgress(45);
+        //seekBarY.setProgress(180);
+        setData(10, 5);
+
+
+
+
+
+
+
+
+
+        /*Intent intent = new Intent(this, LoginActivity.class);
         Log.d(TAG, "startActivity(intent) created");
         startActivity(intent);                                                      // Caldra fer startActibityForResult per saber si ha pogut fer login o no
+        */
     }
 
+    private void setData(int count, float range) {
 
+        ArrayList<Entry> values = new ArrayList<>();
+
+        /*for (int i = 0; i < count; i++) {
+
+            float val = (float) (Math.random() * range) - 30;
+            values.add(new Entry(i, val, getResources().getDrawable(R.drawable.logo)));
+        }*/
+
+        values.add(new Entry(0, 2, getResources().getDrawable(R.drawable.logo)));
+        values.add(new Entry(1, 5, getResources().getDrawable(R.drawable.logo)));
+        values.add(new Entry(2, 3, getResources().getDrawable(R.drawable.logo)));
+        values.add(new Entry(3, 1, getResources().getDrawable(R.drawable.logo)));
+
+        LineDataSet set1;
+
+        if (chart.getData() != null &&
+                chart.getData().getDataSetCount() > 0) {
+            set1 = (LineDataSet) chart.getData().getDataSetByIndex(0);
+            set1.setValues(values);
+            chart.getData().notifyDataChanged();
+            chart.notifyDataSetChanged();
+        } else {
+            // create a dataset and give it a type
+            set1 = new LineDataSet(values, "DataSet 1");
+
+            set1.setMode(LineDataSet.Mode.CUBIC_BEZIER);
+            set1.setCubicIntensity(0.2f);
+            set1.setDrawFilled(true);
+            set1.setDrawCircles(false);
+            set1.setLineWidth(1.8f);
+            set1.setCircleRadius(4f);
+            set1.setCircleColor(Color.WHITE);
+            set1.setHighLightColor(Color.rgb(244, 117, 117));
+            set1.setColor(Color.WHITE);
+            set1.setFillColor(Color.WHITE);
+            set1.setFillAlpha(100);
+            set1.setDrawHorizontalHighlightIndicator(false);
+            set1.setFillFormatter(new IFillFormatter() {
+                @Override
+                public float getFillLinePosition(ILineDataSet dataSet, LineDataProvider dataProvider) {
+                    return chart.getAxisLeft().getAxisMinimum();
+                }
+            });
+
+            // create a data object with the data sets
+            LineData data = new LineData(set1);
+            //data.setValueTypeface(tfLight);
+            data.setValueTextSize(9f);
+            data.setDrawValues(false);
+
+            // set data
+            chart.setData(data);
+        }
+
+    }
 }
