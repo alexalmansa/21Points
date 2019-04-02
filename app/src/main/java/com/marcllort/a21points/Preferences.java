f0375f41bd803929e497f64210f54f14e8b0b707
javoGaig <43567109+javoGaig@users.noreply.github.com>
2019-04-02 10:48:36 +0200

    preferences v1 added

---
+++ app/src/main/java/com/marcllort/a21points/Preferences.java
@@ -0,0 +1,52 @@
+package com.marcllort.a21points;
+
+import com.google.gson.annotations.Expose;
+import com.google.gson.annotations.SerializedName;
+
+public class Preferences {
+    @SerializedName("id")
+    @Expose
+    private Integer id;
+    @SerializedName("user")
+    @Expose
+    private User user;
+    @SerializedName("weeklyGoal")
+    @Expose
+    private Integer weeklyGoal;
+    @SerializedName("weightUnits")
+    @Expose
+    private String weightUnits;
+
+    public Integer getId() {
+        return id;
+    }
+
+    public void setId(Integer id) {
+        this.id = id;
+    }
+
+    public User getUser() {
+        return user;
+    }
+
+    public void setUser(User user) {
+        this.user = user;
+    }
+
+    public Integer getWeeklyGoal() {
+        return weeklyGoal;
+    }
+
+    public void setWeeklyGoal(Integer weeklyGoal) {
+        this.weeklyGoal = weeklyGoal;
+    }
+
+    public String getWeightUnits() {
+        return weightUnits;
+    }
+
+    public void setWeightUnits(String weightUnits) {
+        this.weightUnits = weightUnits;
+    }
+
+}
